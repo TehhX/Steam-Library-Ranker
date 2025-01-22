f0b00d974652a9217dec391fd57ecd7b0b61bfef
Sam <156055395+TehhX@users.noreply.github.com>
2025-01-22 00:58:17 -0500

    Update Window.java

--- src/RankerGUI/Window.java
+++ src/RankerGUI/Window.java
@@ -8,11 +8,20 @@ public class Window extends JFrame {
     static final int FRAME_SIZE_X = 1200;
     static final int FRAME_SIZE_Y = 600;
 
+    /// A variable to ensure that only one Window object can exist for the duration of the program.
+    private static boolean alreadyInitialized = false;
+
     static SceneID currentScene = null;
     static Scene[] sceneArray;
 
     public Window() {
         super("Steam Ranker Window");
+
+        // In case of improper Window use, throw error.
+        if (alreadyInitialized)
+            throw new RuntimeException("Attempted to initialize more than one frame, program terminated.");
+        else
+            alreadyInitialized = true;
 
         // Define the array of scenes
         sceneArray = new Scene[] {
@@ -26,21 +35,17 @@ public class Window extends JFrame {
             add(scene);
 
         // Set up the frame with the input panel
-        frameSetup();
-        changeScene(SceneID.Input);
-    }
-
-    /// Does basic frame setup like setting the size, position and behavior.
-    private void frameSetup() {
         setSize(FRAME_SIZE_X, FRAME_SIZE_Y);
         setLocationRelativeTo(null);
         setResizable(false);
         setIconImage(null); // TODO: Change icon image
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+
+        changeScene(SceneID.Input);
     }
 
-    /// Removes the previous scene if it exists, adds the requested scene.
+    /// Removes the previous scene unless there has been no scenes added yet, adds the requested scene regardless.
     public void changeScene(SceneID nextScene) {
         if (currentScene != null)
             sceneArray[currentScene.ordinal()].removeScene();
