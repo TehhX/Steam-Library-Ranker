f95d3ad5d0f12f69c1f7d5c22368c63afaaac2e1
Sam <156055395+TehhX@users.noreply.github.com>
2025-01-29 15:38:54 -0500

    Update RankerMain.java

--- src/Ranker/RankerMain.java
+++ src/Ranker/RankerMain.java
@@ -6,7 +6,6 @@ import Ranker.GUI.Window;
 public class RankerMain {
     public static void main(String[] args) {
         try {
-            Intake.loadNonGames();
             new Window();
         } catch (Exception e) {
             throw new RuntimeException(e);
